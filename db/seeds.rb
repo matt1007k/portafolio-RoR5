74aea2c1228180d1fd6361d9c7859366e1aba3e3
matt1007k <matt1007k@gmail.com>
2018-05-10 16:37:01 -0500

    Generamos datos para portafolio, blog u habilidades y mostramos los portafolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Praesent magna nisi, porttitor sit amet egestas eu, dapibus ut est. In ut augue est. Morbi sit amet rhoncus metus. Pellentesque vestibulum mi at justo placerat egestas. In bibendum fermentum justo, ac tristique tellus. Fusce vel risus ipsum. In hac habitasse platea dictumst. Cras ultricies mollis risus sit amet fermentum. Vestibulum cursus lacus mi, eget ultrices quam hendrerit eu. Vivamus porta, elit eget accumsan volutpat, velit nisi maximus orci, vitae ultrices mi ante vitae odio."
+    )
+end
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+
+9.times do |portfolio_item| 
+    Portfolio.create!(
+        title: "Portafolio titulo: #{portfolio_item}",
+        subtitle: "My great service",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Praesent magna nisi, porttitor sit amet egestas eu, dapibus ut est. In ut augue est. Morbi sit amet rhoncus metus. Pellentesque vestibulum mi at justo placerat egestas. In bibendum fermentum justo, ac tristique tellus. Fusce vel risus ipsum. In hac habitasse platea dictumst. Cras ultricies mollis risus sit amet fermentum. Vestibulum cursus lacus mi, eget ultrices quam hendrerit eu. Vivamus porta, elit eget accumsan volutpat, velit nisi maximus orci, vitae ultrices mi ante vitae odio.",
+        main_image: "http://placehold.it/600x400",
+        thumb_image: "http://placehold.it/350x200"
+         
+    )
+end
+    
